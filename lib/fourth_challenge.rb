d02944f374234f96095a09fb98ff5b1391c90d79
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-21 22:23:18 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{:name => "Mercutio", :age => "18", :attitude => "hot-headed"
+      :heroine_friends => [{:name => "Mercutio", :age => "18", :attitude => "hot-headed"}, {[:name => "Benvolio", :age => "17", :attitude => "worried"
         #your hashes here!
       ]
    }
